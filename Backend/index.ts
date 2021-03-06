c8234aef3299fec803c7559665aa4736e9c9f8d8
Claudinei Jose Ferreira <claudineijose@gmail.com>
2021-03-06 17:22:16 -0300

    Inclusão do arquivo de rotas

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,7 +1,10 @@
 import app from './config/express';
+import indexRoutes from './src/routers/';
 
 const server = app();
 const port = server.get('port');
+
+server.use(indexRoutes);
 
 server.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
